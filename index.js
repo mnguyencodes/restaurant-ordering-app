f04b9d03505f320864ec1841fc9af40271324bc8
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-02-09 16:26:26 -0600

    Add and style order section.

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function getHtmlString() {
         } = item
 
         return `
-        <div class="container">
+        <div class="container-menu">
             <img src="${location}">
-            <div class="item">
-                <h2 class="mt0 mb0">${name}</h2>
-                <p class="mb0 item-ingredients">${ingredients.join(", ")}</p>
+            <div class="menu-item">
+                <h2 class="mt0 mb0 subheading">${name}</h2>
+                <p class="mb0 menu-item-ingredients">${ingredients.join(", ")}</p>
                 <p class="mb0 item-amt">$${price}</p>
             </div>
             <button data-pizza-${id} class="btn add-btn"><i class="fa-solid fa-plus"></i></button>
